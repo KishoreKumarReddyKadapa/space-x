002f45108d84299a17b740adbbb4c4a26f203bdb
kishore reddy kadapa <kishorekumarreddykadapa@gmail.com>
2021-08-24 17:41:54 +0530

    Added Header Component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from './Components/Header/Header.jsx';
+import './style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
